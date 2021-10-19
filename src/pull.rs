261d0d0f8225a4b3a7b91ce805fe121ac8870964
raulcabello <raul.cabello@suse.com>
2021-10-19 15:42:08 +0200

    Add latest tag if not present when pulling from a registry

--- src/pull.rs
+++ src/pull.rs
@@ -10,5 +10,50 @@ pub(crate) async fn pull(
     sources: Option<Sources>,
     destination: PullDestination,
 ) -> Result<PathBuf> {
-    fetch_policy(uri, destination, docker_config, sources.as_ref()).await
+    let uri = add_latest_if_tag_not_present(uri);
+    fetch_policy(&uri, destination, docker_config, sources.as_ref()).await
+}
+
+fn add_latest_if_tag_not_present(uri: &str) -> String {
+    if is_registry_and_does_not_contain_tag(uri) {
+        let latest_tag = "latest";
+        [uri, latest_tag].join(":")
+    } else {
+        uri.to_string()
+    }
+}
+
+fn is_registry_and_does_not_contain_tag(uri: &str) -> bool {
+    let v: Vec<&str> = uri.split(':').collect();
+    v[0] == "registry" && v.len() == 2
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::pull::add_latest_if_tag_not_present;
+
+    #[test]
+    fn test_latest_tag_is_added_if_tag_not_present() {
+        let uri = "registry://ghcr.io/kubewarden/policies/psp-capabilities";
+        let uri_expected = "registry://ghcr.io/kubewarden/policies/psp-capabilities:latest";
+        assert_eq!(uri_expected, add_latest_if_tag_not_present(uri))
+    }
+
+    #[test]
+    fn test_latest_tag_is_not_added_if_tag_present() {
+        let uri = "registry://ghcr.io/kubewarden/policies/psp-capabilities:v1";
+        assert_eq!(uri, add_latest_if_tag_not_present(uri))
+    }
+
+    #[test]
+    fn test_latest_tag_is_not_added_if_not_registry() {
+        let uri = "https://github.com/kubewarden/pod-privileged-policy/releases/download/v0.1.9/policy.wasm";
+        assert_eq!(uri, add_latest_if_tag_not_present(uri))
+    }
+
+    #[test]
+    fn test_latest_tag_empty_uri() {
+        let uri = "";
+        assert_eq!(uri, add_latest_if_tag_not_present(uri))
+    }
 }
